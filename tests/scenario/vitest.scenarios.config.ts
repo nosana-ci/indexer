4a9c4f152aa638304d48409bec5133b01d26308f
Jairzijp <jairzijp123@gmail.com>
2026-09-07 13:40:27 +0200

    fix: scenario tests

--- tests/scenario/vitest.scenarios.config.ts
+++ tests/scenario/vitest.scenarios.config.ts
@@ -45,7 +45,6 @@ export default () => {
       env: {
         BACKEND_URL: process.env.BACKEND_URL ?? "http://localhost:3003",
         CRON_URL: process.env.CRON_URL ?? "http://localhost:3005",
-        SOLANA_NETWORK: process.env.SOLANA_NETWORK ?? "mainnet",
         NOSANA_NETWORK: "localnet",
         SOLANA_RPC: process.env.SOLANA_RPC ?? "http://localhost:8899",
         SOLANA_WS: process.env.SOLANA_WS ?? "ws://localhost:8900",
